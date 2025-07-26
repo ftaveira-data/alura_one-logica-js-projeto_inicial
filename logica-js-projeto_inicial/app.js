edb740c746cf15a4955d13e285776ea7df89e9ba
Francisco Taveira <ftaveira.dev@gmail.com>
2025-07-26 17:55:03 -0300

    init commit

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,21 @@
+//alert("Olá Mundo");
+//let numeroSecreto = 45;
+//let chute = prompt("Escolha um número entre 1 e 50");
+//if (chute == numeroSecreto) {
+//    console.log("Isso ai! Você descobriu o número secreto (45)");
+//}
+
+//1// alert("Boas Vindas ao nosso site!");
+//2// let nome = "Lua";
+//3// let idade = 25;
+//4// let numeroDeVendas = 50;
+//5// let saldoDisponivel = 1000;
+//6// alert("Erro! Preencha todos os campos")
+//7//let mensagemDeErro = "Erro! Preencha todos os campos";
+//alert(mensagemDeErro);
+
+let nomeUsuario = prompt("Qual o seu nome?");
+let idadeUsuario = prompt("Qual tua idade?")
+if (idadeUsuario >= 18) {
+    alert("Maior de 18 pode tirar habilitação")
+}
