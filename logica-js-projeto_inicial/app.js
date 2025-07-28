aa1767b2dfe6a3a50090ffebd95fc583bacb646f
Francisco Taveira <ftaveira.dev@gmail.com>
2025-07-28 19:48:45 -0300

    novos estudos sobre javascritp

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,9 +1,13 @@
-//alert("Olá Mundo");
-//let numeroSecreto = 45;
-//let chute = prompt("Escolha um número entre 1 e 50");
-//if (chute == numeroSecreto) {
-//    console.log("Isso ai! Você descobriu o número secreto (45)");
-//}
+alert("Olá Mundo");
+alert("Bem vindo ao jogo do Numero Secreto")
+let numeroSecreto = 45;
+console.log(numeroSecreto)
+let chute = prompt("Escolha um número entre 1 e 50");
+if (chute == numeroSecreto) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+} else {
+    alert("Você errou :(")
+}é
 
 //1// alert("Boas Vindas ao nosso site!");
 //2// let nome = "Lua";
@@ -14,8 +18,10 @@
 //7//let mensagemDeErro = "Erro! Preencha todos os campos";
 //alert(mensagemDeErro);
 
-let nomeUsuario = prompt("Qual o seu nome?");
-let idadeUsuario = prompt("Qual tua idade?")
-if (idadeUsuario >= 18) {
-    alert("Maior de 18 pode tirar habilitação")
-}
+//let nomeUsuario = prompt("Qual o seu nome?");
+//let idadeUsuario = prompt("Qual tua idade?")
+//if (idadeUsuario >= 18) {
+//    alert("Maior de 18 pode tirar habilitação")
+//} 
+
+
