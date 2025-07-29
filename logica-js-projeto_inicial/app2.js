fe4d2b22932074d620107bcfc46b3c649febc443
Francisco Taveira <ftaveira.dev@gmail.com>
2025-07-29 17:35:42 -0300

    exercicios loop javascript

---
+++ logica-js-projeto_inicial/app2.js
@@ -0,0 +1,34 @@
+//let nome = prompt("Digite seu nome:");
+//let mensagem = ("Bem vindo(a) " + nome + " ao nosso site!");
+//alert(mensagem);
+
+//Loops e tentativas
+
+//1//
+//let contador = 1;
+//while (contador <= 10) {
+//    alert(`excutando iteração ` + contador);
+//    contador++;
+//}
+
+//2//
+//let contador = 10;
+//while (contador >=0) {
+//    alert(`Executando iteração ${contador}`)
+//    contador--;
+//}
+
+//3// 
+//let contador = prompt("Digite um numero de 1 a 10:");
+//while (contador >=0) {
+//    console.log(`Executando iteração ${contador}`);
+//    contador--;
+//};
+
+//4// 
+let limite = prompt("Digite um numero de 0 a 20:");
+let contador = 0
+while (contador <= limite) {
+    console.log(`Contando ${contador}`); 
+    contador++;
+} 
