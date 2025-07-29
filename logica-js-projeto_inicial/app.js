fbc837d1491f52712a17c776b30f534823c82bb6
Francisco Taveira <ftaveira.dev@gmail.com>
2025-07-29 00:42:11 -0300

    Desafio de condicionais e concatenacao.

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -41,3 +41,31 @@
 //    alert("Dia inválido, por favor insira um dia da semana válido.");
 //}
 
+//2//
+//let numero = prompt("Digite um numero:");
+//if (numero >= 0) {
+//    alert(`O número ${numero} é positivo`);
+//} else {
+//    alert(`O número ${numero} é negativo`);
+//}
+
+//3//
+//let numero1 = prompt("Digite o primeiro numero:");
+//let numero2 = prompt("Digite o segundo numero:");
+//numero1 = Number(numero1);
+//numero2 = Number(numero2);
+//if (numero1 + numero2 >= 100){
+//    alert("Parabens você ganhou!!"); 
+//}else {
+//    alert("Tente novamente, para ganhar!");
+//}
+
+//4//
+//let saldoDisponivel = 1000;
+//let mensagem = `Seu saldo atual é de R$ ${saldoDisponivel}.`;
+//alert(mensagem);
+
+//5//
+//let nome = prompt("Digite seu nome:");
+//let mensagem = ("Bem vindo(a) " + nome + " ao nosso site!");
+//alert(mensagem);
